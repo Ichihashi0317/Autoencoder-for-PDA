921ea75f33f7731ce0d69d967f654b33d1c39165
Ichihashi0317 <103499611+Ichihashi0317@users.noreply.github.com>
2025-02-21 07:34:34 +0900

    Delete PDA_combined_pkg directory

--- PDA_combined_pkg/__init__.py
+++
@@ -1,4 +0,0 @@
-from .storage import Storage
-from .set_noise_param import set_noise_param
-from .train import train_model
-from .test import test_model
--- PDA_combined_pkg/set_noise_param.py
+++
@@ -1,6 +0,0 @@
-import math
-
-def set_noise_param(SIM, EsN0_dB):
-    EsN0 = 10.0**(EsN0_dB / 10.0)
-    SIM.N0 = SIM.M / EsN0
-    SIM.sigma_noise = math.sqrt(SIM.N0 / 2.0)
